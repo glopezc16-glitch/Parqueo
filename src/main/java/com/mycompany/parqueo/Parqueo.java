4cf6e0f5ecae13c94d56db81fb599c4f46b24661
glopezc16-glitch <glopezc16@miumg.edu.gt>
2025-10-18 20:00:57 -0600

    Primer commit

---
+++ src/main/java/com/mycompany/parqueo/Parqueo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.parqueo;
+
+/**
+ *
+ * @author Shily
+ */
+public class Parqueo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
